eca5709c2c3d1d81b1a44284dbc0873feaa5be14
Patrick Bassner <bassner@in.tum.de>
2017-05-07 17:13:10 +0200

    Added project structure and dummy UI class.

---
+++ src/main/java/com/shelly0/dmxduo/ui/Main.java
@@ -0,0 +1,17 @@
+package com.shelly0.dmxduo.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JFrame frm = new JFrame("Test");
+        frm.setSize(new Dimension(600,400));
+        frm.setLocationRelativeTo(null);
+        frm.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frm.setVisible(true);
+    }
+}
